22477733f1b85fbc0fe9ec6ea9b421fddc8512af
Patrick Mueller <patrick@mueller-patrick.tech>
2022-03-02 21:20:24 +0100

    Adding KDoc

--- src/main/kotlin/com/p4ddy/paddycrm/domain/account/Account.kt
+++ src/main/kotlin/com/p4ddy/paddycrm/domain/account/Account.kt
@@ -3,6 +3,18 @@ package com.p4ddy.paddycrm.domain.account
 import com.p4ddy.paddycrm.domain.address.AddressVO
 import java.time.LocalDate
 
+/**
+ * Account domain entity
+ *
+ * @property name The account name
+ * @property ownerId The id of the user that owns this record
+ * @property billingAddress AddressVO Object of the billing address
+ * @property shippingAddress AddressVO Object of the shipping address. Leave empty to use billing address
+ * @property accountId The unique ID of the account
+ * @property createdDate The date of the account creation
+ *
+ * @constructor Creates a new account entity
+ */
 class Account(
 	name: String,
 	ownerId: Int,
--- src/main/kotlin/com/p4ddy/paddycrm/domain/account/AccountRepo.kt
+++ src/main/kotlin/com/p4ddy/paddycrm/domain/account/AccountRepo.kt
@@ -1,13 +1,31 @@
 package com.p4ddy.paddycrm.domain.account
 
+/**
+ * Account repo interface
+ */
 interface AccountRepo {
+	/**
+	 * Find all accounts (that are visible to the current user)
+	 */
 	fun findAll(): List<Account>
 
+	/**
+	 * Find the account by the given id (if it is visible to the current user)
+	 */
 	fun findById(id: Int): Account
 
+	/**
+	 * Save the given account to the database
+	 */
 	fun save(acct: Account): Account
 
+	/**
+	 * Update the given account in the database
+	 */
 	fun update(acct: Account): Account
 
+	/**
+	 * Delete the given account in the database
+	 */
 	fun delete(acct: Account)
 }
