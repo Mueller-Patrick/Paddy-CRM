bdecfdbd8da701adc7e7ee02cc7e094b56d1636a
Patrick Mueller <patrick@mueller-patrick.tech>
2022-03-06 17:33:52 +0100

    Automatically setting contact createdDate

--- src/main/kotlin/com/p4ddy/paddycrm/domain/contact/Contact.kt
+++ src/main/kotlin/com/p4ddy/paddycrm/domain/contact/Contact.kt
@@ -1,30 +1,31 @@
 package com.p4ddy.paddycrm.domain.contact
 
 import com.p4ddy.paddycrm.domain.address.AddressVO
+import java.time.LocalDate
 import java.util.*
 
 /**
  * Contact domain entity
  *
  * @property accountId The id of the related account
- * @property createdDate The date of the account creation
  * @property ownerId The id of the user that owns this record
  * @property salutation The salutation of the contact
  * @property lastName The last name of the contact
  * @property firstName The first name of the contact
  * @property address AddressVO Object of the contact address
  * @property contactId The unique id of the contact
+ * @property createdDate The date of the account creation
  *
  * @constructor Creates a new contact entity
  */
 class Contact(
 	val accountId: Int,
-	val createdDate: Date,
 	val ownerId: Int,
 	var salutation: String,
 	var lastName: String,
 	var firstName: String,
 	var address: AddressVO,
-	val contactId: Int = -1
+	val contactId: Int = -1,
+	val createdDate: LocalDate = LocalDate.now()
 ) {
 }
